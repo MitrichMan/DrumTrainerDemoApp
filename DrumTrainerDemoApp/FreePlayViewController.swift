7cf67152db87a99c8a2f1d972d98ae9fd360a456
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-02-03 17:39:46 +0800

    Seguees setup

---
+++ DrumTrainerDemoApp/FreePlayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FreePlayViewController.swift
+//  DrumTrainerDemoApp
+//
+//  Created by Dmitrii Melnikov on 03.02.2023.
+//
+
+import UIKit
+
+class FreePlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
