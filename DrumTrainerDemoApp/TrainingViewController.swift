6c8f05b36e74c4de0fb32b1a68938948c2e12bcf
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-02-07 20:14:46 +0800

    Help view for training section was made.

--- DrumTrainerDemoApp/TrainingViewController.swift
+++ DrumTrainerDemoApp/TrainingViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class TrainingViewController: UIViewController {
 
+    @IBOutlet var openHelpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+    performSegue(withIdentifier: "goToSectionHelpVC", sender: nil)
+        
     }
-    */
-
+    
+    @IBAction func openHelpButtonTapped() {
+        performSegue(withIdentifier: "goToSectionHelpVC", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToTrainingVC(segue: UIStoryboardSegue) {
+        
+    }
+    
+    
 }
+
+
